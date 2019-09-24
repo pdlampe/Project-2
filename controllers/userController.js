2d95bb373ab8d44a39ebbf6f1a7f021efdea7d5a
Colin Elving <viewtifuljho@hotmail.com>
2019-09-24 16:23:03 -0500

    set up models, controllers, and middleware for user authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,99 @@
-var db = require("../models");
+var { User } = require("../models");
+var express = require("express");
 var Sequelize = require("sequelize");
 var Op = Sequelize.Op;
+var bcrypt = require("bcrypt");
 
-module.exports = function(app) {
-  app.get("/api/users/:user", function(req, res) {
-    db.User.findOne({
-      where: {
-        userName: req.params.user
-      }
-    }).then(function(result) {
-      res.json(result);
-    });
+
+module.exports = function (app) {
+  //Register route//
+  app.post('/register', async (req, res) => {
+
+    // hash the password provided by the user with bcrypt so that
+    // we are never storing plain text passwords. This is crucial
+    // for keeping your db clean of sensitive data
+    const hash = bcrypt.hashSync(req.body.password, 10);
+
+    try {
+      // create a new user with the password hash from bcrypt
+      let user = await User.create(
+        Object.assign(req.body, { password: hash })
+      );
+
+      // data will be an object with the user and it's authToken
+      let data = await user.authorize();
+
+      // send back the new user and auth token to the
+      // client { user, authToken }
+      return res.json(data);
+
+    } catch (err) {
+      return res.status(400).send(err);
+    }
+
+  });
+
+  /* Login Route
+========================================================= */
+  app.post('/login', async (req, res) => {
+    const { userName, password } = req.body;
+
+    // if the username / password is missing, we use status code 400
+    // indicating a bad request was made and send back a message
+    if (!userName || !password) {
+      return res.status(400).send(
+        'Request missing username or password param'
+      );
+    }
+
+    try {
+      let user = await User.authenticate(userName, password)
+
+      user = await user.authorize();
+
+      return res.json(user);
+
+    } catch (err) {
+      return res.status(400).send('invalid username or password');
+    }
+
+  });
+
+  /* Logout Route
+========================================================= */
+  app.delete('/logout', async (req, res) => {
+
+    // because the logout request needs to be send with
+    // authorization we should have access to the user
+    // on the req object, so we will try to find it and
+    // call the model method logout
+    const { user, cookies: { auth_token: authToken } } = req
+
+    // we only want to attempt a logout if the user is
+    // present in the req object, meaning it already
+    // passed the authentication middleware. There is no reason
+    // the authToken should be missing at this point, check anyway
+    if (user && authToken) {
+      await req.user.logout(authToken);
+      return res.status(204).send()
+    }
+
+    // if the user missing, the user is not logged in, hence we
+    // use status code 400 indicating a bad request was made
+    // and send back a message
+    return res.status(400).send(
+      { errors: [{ message: 'not authenticated' }] }
+    );
+  });
+
+  /* Me Route - get the currently logged in user
+========================================================= */
+  app.get('/me', (req, res) => {
+    if (req.user) {
+      return res.send(req.user);
+    }
+    res.status(404).send(
+      { errors: [{ message: 'missing auth token' }] }
+    );
   });
 };
--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+var bcrypt = require("bcrypt");
+
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
         userName: {
@@ -28,5 +30,46 @@ module.exports = function (sequelize, DataTypes) {
     User.associate = function(models) {
         User.hasMany(models.AuthToken);
     };
+
+    User.authenticate = async function(userName, password) {
+
+        const user = await User.findOne({ where: { userName } });
+    
+        // bcrypt is a one-way hashing algorithm that allows us to 
+        // store strings on the database rather than the raw
+        // passwords. Check out the docs for more detail
+        if (bcrypt.compareSync(password, user.password)) {
+          return user.authorize();
+        }
+    
+        throw new Error('invalid password');
+      }
+    
+      // in order to define an instance method, we have to access
+      // the User model prototype. This can be found in the
+      // sequelize documentation
+      User.prototype.authorize = async function () {
+        const { AuthToken } = sequelize.models;
+        const user = this
+    
+        // create a new auth token associated to 'this' user
+        // by calling the AuthToken class method we created earlier
+        // and passing it the user id
+        const authToken = await AuthToken.generate(this.id);
+    
+        // addAuthToken is a generated method provided by
+        // sequelize which is made for any 'hasMany' relationships
+        await user.addAuthToken(authToken);
+    
+        return { user, authToken }
+      };
+    
+    
+      User.prototype.logout = async function (token) {
+    
+        // destroy the auth token record that matches the passed token
+        sequelize.models.AuthToken.destroy({ where: { token } });
+      };
+    
     return User;
 }
