67211bca2b7d0f4308f6aed1e3824e16d691c828
Paul David Lampe <pdlampe@gmail.com>
2019-09-24 21:40:35 -0500

    Updated CSS and Handlebars files

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Th1sissc@ry",
+    password: "",
     database: "produce_db"
   });
 }
